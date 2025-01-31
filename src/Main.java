1d366f7f4614b19db5bbbedd611ee032b9a942de
Albert <72774907+AlbertSimaoS@users.noreply.github.com>
2025-01-31 15:27:24 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        ContaBancaria conta = new ContaBancaria();
+
+        conta.depositar(1000);
+        conta.consultarSaldo();
+
+        ContaCorrente contaCorrente = new ContaCorrente();
+        contaCorrente.depositar(200);
+        contaCorrente.cobrarTarifaMensal();
+        contaCorrente.consultarSaldo();
+        contaCorrente.sacar(150);
+        contaCorrente.consultarSaldo();
     }
 }
